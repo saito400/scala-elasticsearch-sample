2b158dfc615309f353ab70803ee4935d4fbeb524
saito400 <ken.s.530000@gmail.com>
2015-04-12 21:04:26 +0900

    c

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,39 +1,45 @@
 package main {
 
   import jp.co.bizreach.elasticsearch4s._
+  import org.elasticsearch.index.query.QueryBuilders
+  import models._
 
   object Main {
 
-    case class Tweet(name: String, message: String)
+//    case class Tweet(name: String, message: String)
 
     def main(args: Array[String]) {
       println("start")
 
-//      ESClient.using("http://localhost:9200") { client =>
-//        val config = ESConfig("twitter", "tweet")
-//
-//        // Insert
-//        client.insert(config, Tweet("takezoe", "Hello World!!"))
+      ESClient.using("http://localhost:9200") { client =>
+        val config = ESConfig("twitter", "tweet")
+
+        // Insert
+        client.insert(config, Tweet(Some("John doe"), Some("Hello World!!")))
 //        client.insertJson(config, """{name: "John doe", message: "Hello World!!"}""")
-//
-//        // Update
-//        client.update(config, "1", Tweet("takezoe", "Hello Scala!!"))
+
+        // Update
+        client.update(config, "1", Tweet(Some("Jane doe"), Some("Hello Scala!!")))
 //        client.updateJson(config, "1", """{name: "Jane doe", message: "Hello World!!"}""")
-//
-//        // Delete
-//        client.delete(config, "1")
-//
-//        // Find one document
-//        val tweet: Option[(String, Tweet)] = client.find[Tweet](config) { seacher =>
-//          seacher.setQuery(QueryBuilders.termQuery("_id", "1"))
-//        }
-//
-//        // Search documents
+
+        // Delete
+        client.delete(config, "1")
+
+        // Find one document
+        val tweet: Option[(String, Tweet)] = client.find[Tweet](config) { seacher =>
+          seacher.setQuery(QueryBuilders.termQuery("_id", "1"))
+        }
+
+        // Search documents
 //        val list: List[ESSearchResult] = client.list[Tweet](config) { seacher =>
-//          seacher.setQuery(QueryBuilders.termQuery("name", "takezoe"))
-//        }
-//      }
-//
+//        val list: List[ESSearchResult[Tweet]] = client.list[Tweet](config) { seacher =>
+        val list = client.list[Tweet](config) { seacher =>          seacher.setQuery(QueryBuilders.termQuery("name", "John doe"))
+        }
+
+        println(list)
+
+      }
+
       println("end")
     }
   }

af241cdcda5b6ef88003d35793cb0bb994e6624f
saito400 <ken.s.530000@gmail.com>
2015-04-12 20:38:58 +0900

    c

--- src/main/scala/models/Tweet.scala
+++ src/main/scala/models/Tweet.scala
@@ -3,7 +3,6 @@ import Tweet._
 
 case class Tweet(
   message: Option[String], 
-  post_date: Option[org.joda.time.DateTime], 
   user: Option[String]
 )
 
@@ -13,7 +12,6 @@ object Tweet {
 
 
   val message = "message"
-  val post_date = "post_date"
   val user = "user"
 
 }

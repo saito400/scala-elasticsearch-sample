4117b8a182082c5b29de3a927552598eaaccdc9c
saito400 <ken.s.530000@gmail.com>
2015-04-12 22:02:41 +0900

    c

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -15,17 +15,18 @@ package main {
         val config = ESConfig("twitter", "tweet")
 
         // Insert
-        client.insert(config, Tweet(Some("John doe"), Some("Hello World!")))
-        client.insert(config, Tweet(Some("John doe"), Some("Hello World!!")))
-        client.insert(config, Tweet(Some("John doe"), Some("Hello World!!!")))
+        client.insert(config, Tweet(user = Some("John doe"), message = Some("Hello World!")))
+        client.insert(config, Tweet(user = Some("John doe"), message = Some("Hello World!!")))
+        client.insert(config, Tweet(user = Some("John doe"), message = Some("Hello World!!!")))
+        client.insert(config, Tweet(user = Some("John doe"), message = Some("Hello World!!!!")))
 //        client.insertJson(config, """{name: "John doe", message: "Hello World!!"}""")
 
         // Update
-        client.update(config, "1", Tweet(Some("Jane doe"), Some("Hello Scala!!")))
+//        client.update(config, "1", Tweet(Some("Jane doe"), Some("Hello Scala!!")))
 //        client.updateJson(config, "1", """{name: "Jane doe", message: "Hello World!!"}""")
 
         // Delete
-        client.delete(config, "1")
+//        client.delete(config, "1")
 
         // Find one document
         val tweet: Option[(String, Tweet)] = client.find[Tweet](config) { seacher =>
@@ -35,9 +36,9 @@ package main {
         // Search documents
 //        val list: List[ESSearchResult] = client.list[Tweet](config) { seacher =>
 //        val list: List[ESSearchResult[Tweet]] = client.list[Tweet](config) { seacher =>
-//        val list = client.list[Tweet](config) { seacher =>          
-        val list: ESSearchResult[Tweet] = client.list[Tweet](config) { seacher =>
-          seacher.setQuery(QueryBuilders.termQuery("name", "John doe"))
+        val list = client.list[Tweet](config) { seacher =>          
+//        val list: ESSearchResult[Tweet] = client.list[Tweet](config) { seacher =>
+          seacher.setQuery(QueryBuilders.termQuery("user", "John doe"))
         }
 
         println(list)
